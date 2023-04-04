b0251436e9ecbd570c4f291f5f1f09afbce08734
liviu-vlad <liviu.vlad@innovatorspark.com>
2023-04-04 18:47:46 +0300

    fix for mumbai

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,47 @@
 import type { NextPage } from "next";
-import styles from "../styles/Home.module.css";
 import abi from "src/utils/abi.json";
 import Web3 from "web3";
 import { useState } from "react";
-import React from "react";
+import { BigNumber } from "ethers";
 
 const Home: NextPage = () => {
+  const privateKey =
+    "e5ade216df5a68ff325602f25a0ecc0b182334998690e528f3397ee7ac8b7c1c"; // should be in .env
   const [voucher, setVoucher] = useState();
-  const web3 = new Web3("http://localhost:8545");
+  const mumbaiRPC = "https://polygon-mumbai.infura.io/v3/4458cf4d1689497b9a38b1d6bbf05e78"; // should be in .env
+  const web3 = new Web3(mumbaiRPC);
   const wiiqarecontract = new web3.eth.Contract(
     abi.abi,
-    "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+    "0xd55Fc611D64f10885f243dbAcA1757349FcE2Bc7" // should be in .env
   );
+  const web3Account = web3.eth.accounts.privateKeyToAccount(privateKey);
+
+  const getGasFees = async () => {
+    return (await fetch("https://gasstation-mumbai.matic.today/v2")).json();
+  };
 
   const mintVoucher = async () => {
     try {
-      const accounts = await web3.eth.getAccounts();
+       const gasParams = await getGasFees();
+      // const resSigned = await web3.eth.accounts.signTransaction(
+      //   {
+      //     to: "0xd55Fc611D64f10885f243dbAcA1757349FcE2Bc7",
+      //     gas: web3.utils.toWei(gasParams.standard.maxFee.toString()).toString(),
+      //   },
+      //   privateKey
+      // );
+      //const accounts = await web3.eth.getAccounts();
+      const rr = web3.eth.accounts.wallet.add(privateKey);
       const res = await wiiqarecontract.methods
         .mintVoucher([
-          200,
+          101,
           "USD",
-          "ownerDemo",
-          "hospitalDemo",
+          "wiiqareDemo",
+          "hospitalA",
           "pacientDemo",
           "notClaimed",
         ])
-        .send({ from: accounts[0] });
+        .send({ from: web3Account.address, gasPrice: '30000000000', gas: '9996000'});
       console.log(res);
     } catch (err) {
       console.error("Contract call failure", err);
@@ -36,8 +52,8 @@ const Home: NextPage = () => {
     try {
       const accounts = await web3.eth.getAccounts();
       const res = await wiiqarecontract.methods
-        .vouchers(1)
-        .call({ from: accounts[0] });
+        .vouchers(4)
+        .call({ from: web3Account.address});
       setVoucher(res);
     } catch (err) {
       console.error("Contract call failure", err);
