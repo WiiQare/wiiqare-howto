c1542d3f68025a2b2a2b31521366017d352ad940
liviu-vlad <liviu.vlad@innovatorspark.com>
2023-06-23 19:36:10 +0300

    how-to: split voucher

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,14 +8,14 @@ const Home: NextPage = () => {
   const privateKey =
     "e5ade216df5a68ff325602f25a0ecc0b182334998690e528f3397ee7ac8b7c1c"; // should be in .env
   const [voucher, setVoucher] = useState();
-  const mumbaiRPC = "https://polygon-mumbai.infura.io/v3/4458cf4d1689497b9a38b1d6bbf05e78"; // should be in .env
+  const mumbaiRPC =
+    "https://polygon-mumbai.infura.io/v3/4458cf4d1689497b9a38b1d6bbf05e78"; // should be in .env
   const web3 = new Web3(mumbaiRPC);
   const wiiqarecontract = new web3.eth.Contract(
     abi.abi,
     "0xd55Fc611D64f10885f243dbAcA1757349FcE2Bc7" // should be in .env
   );
   const web3Account = web3.eth.accounts.privateKeyToAccount(privateKey);
-
 
   const mintVoucher = async () => {
     try {
@@ -29,7 +29,11 @@ const Home: NextPage = () => {
           "pacientDemo",
           "notClaimed",
         ])
-        .send({ from: web3Account.address, gasPrice: '30000000000', gas: '3996000'});
+        .send({
+          from: web3Account.address,
+          gasPrice: "30000000000",
+          gas: "3996000",
+        });
       console.log(res);
     } catch (err) {
       console.error("Contract call failure", err);
@@ -40,8 +44,8 @@ const Home: NextPage = () => {
     try {
       const accounts = await web3.eth.getAccounts();
       const res = await wiiqarecontract.methods
-        .vouchers(167)
-        .call({ from: web3Account.address});
+        .vouchers(182)
+        .call({ from: web3Account.address });
       setVoucher(res);
     } catch (err) {
       console.error("Contract call failure", err);
@@ -60,7 +64,45 @@ const Home: NextPage = () => {
           "pacientDemo",
           "notClaimed",
         ])
-        .send({ from: web3Account.address, gasPrice: '30000000000', gas: '3996000'});
+        .send({
+          from: web3Account.address,
+          gasPrice: "30000000000",
+          gas: "3996000",
+        });
+      console.log(res);
+    } catch (err) {
+      console.error("Contract call failure", err);
+    }
+  };
+
+  const splitVoucher = async () => {
+    try {
+      const signResponse = web3.eth.accounts.wallet.add(privateKey);
+      const res = await wiiqarecontract.methods
+        .splitVoucher(
+          175,
+          [
+            50,
+            "RON",
+            "demoWiiAlter1",
+            "hospitalWii1",
+            "pacientDemo1",
+            "notClaimed",
+          ],
+          [
+            60,
+            "RON",
+            "demoWiiAlter2",
+            "hospitalWii2",
+            "pacientDemo2",
+            "notClaimed",
+          ]
+        )
+        .send({
+          from: web3Account.address,
+          gasPrice: "30000000000",
+          gas: "3996000",
+        });
       console.log(res);
     } catch (err) {
       console.error("Contract call failure", err);
@@ -72,6 +114,7 @@ const Home: NextPage = () => {
       <div>
         <button onClick={mintVoucher}>Mint voucher</button>
         <button onClick={getVoucher}>Get voucher voucher</button>
+        <button onClick={splitVoucher}>Split voucher</button>
         <div>
           <ul>
             <li>{voucher.value}</li>
@@ -90,6 +133,7 @@ const Home: NextPage = () => {
         <button onClick={mintVoucher}>Mint voucher</button>
         <button onClick={getVoucher}>Get voucher voucher</button>
         <button onClick={alterVoucher}>Alter voucher</button>
+        <button onClick={splitVoucher}>Split voucher</button>
       </div>
     );
   }
